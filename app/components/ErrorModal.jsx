d475e85f2d410b589571c0db147ff961571c5536
Steven Simonitch <steven.simonitch@tastemade.com>
2017-05-24 10:38:39 -0700

    Added error modal 4 realz this time

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,38 @@
+const React = require('react');
+
+let ErrorModal = React.createClass({
+  getDefaultProps: function() {
+    return {
+      title: 'Error',
+    };
+  },
+
+  // optional & required prop types
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired,
+  },
+
+  componentDidMount: function() {
+    let modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+
+  render: function() {
+    let {title, message} = this.props;
+
+    return (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>Error: {message}</p>
+        <p>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
+        </p>
+      </div>
+    );
+  }
+});
+
+module.exports = ErrorModal;
