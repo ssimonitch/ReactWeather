c44c2db10db78c1c659c919f7b97ccdf8df12a08
Steven Simonitch <steven.simonitch@tastemade.com>
2017-05-24 09:32:43 -0700

    Updated example and about pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 const Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out!</p>
+      <ol>
+        <li>
+          <Link to='/?location=Los Angeles'>Los Angeles, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Tokyo'>Tokyo, Japan</Link>
+        </li>
+      </ol>
     </div>
   )
 };
