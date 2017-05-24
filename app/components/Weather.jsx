80d6ef744b43af39bb2d53550d7d3663bb6e469f
Steven Simonitch <steven.simonitch@tastemade.com>
2017-05-24 09:36:42 -0700

    Updated styles on weather page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -36,7 +36,7 @@ const Weather = React.createClass({
     // conditionally render inside of component, must use jsx
     function renderMessage() {
       if (isLoading) {
-        return <h3>Fetching weather...</h3>;
+        return <h3 className="text-center">Fetching weather...</h3>;
       } else if (temp && location) {
         return <WeatherMessage temp={temp} location={location} />;
       }
@@ -44,7 +44,7 @@ const Weather = React.createClass({
 
     return (
       <div>
-        <h3>Weather Component</h3>
+        <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch} />
         {renderMessage()}
       </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ const React = require('react');
 const WeatherMessage = ({temp, location}) => {
 
   return (
-    <h3>It is {temp} in {location}.</h3>
+    <h3 className="text-center">It is {temp} in {location}.</h3>
   )
 }
 
